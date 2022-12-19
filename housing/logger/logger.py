ae4efc2c9a32dddcadb6ff644fbe89681061a764
ashishs1407 <shimpi.ashish.me2@gmail.com>
2022-12-20 00:12:29 +0530

    updated

--- housing/logger/logger.py
+++ housing/logger/logger.py
@@ -1,9 +1,10 @@
 import os
 import logging
-from datetime import datetime
 from from_root import from_root
+from datetime import datetime
 
-log_file = f'datetime.now().strftime(%d_%m_%y_%H_%M_%S).log'
+log_file = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.log"
+#log_file = f"log_{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.log"
 log_path = os.path.join(from_root() , "log",log_file)
 os.mkdirs(log_path,exist_ok = True)
 log_file_path = os.path.join(log_path)
