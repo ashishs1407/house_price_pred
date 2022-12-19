fccda0f15be9213935626f8dbeea9927b644a845
ashishs1407 <shimpi.ashish.me2@gmail.com>
2022-12-20 01:52:35 +0530

    updates

--- demo.py
+++ demo.py
@@ -1,12 +1,18 @@
 from housing.constants import db_name, db_url
 from housing.database import connection
-import os
+from housing.logger.housing_logger import logging
+from housing.exception.housing_exception import HousingException
+import sys,os
 
 
 
 if __name__ == "__main__":
-    conn = connection.connection_client()
-    print("databases are as 123 : " ,conn.client[db_name])
-    print("databases are as 456: " ,conn)
+    try :
+        logging.info("connecting to mongoDB")
+        conn = connection.connection_client()
+        print("databases are as 123 : " ,conn.client[db_name])
+        print("databases are as 456: " ,conn)
+    except Exception as e:
+        raise HousingException(e,sys)
     
     
--- housing/logger/housing_logger.py
+++ housing/logger/housing_logger.py
@@ -3,17 +3,18 @@ import logging
 from from_root import from_root
 from datetime import datetime
 
+
 log_file = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.log"
 #log_file = f"log_{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.log"
 log_path = os.path.join(from_root() , "log",log_file)
-os.mkdirs(log_path,exist_ok = True)
-log_file_path = os.path.join(log_path)
+os.makedirs(log_path,exist_ok = True)
+log_file_path = os.path.join(log_path,log_file)
 
 logging.basicConfig(
     filename= log_file_path,
-    format = '[%(asctime)s] %(lineno)d %(name)s %(levelname)s %(message)s',
-    level = logging.info
-    
+    filemode="w+",
+    format = "[%(asctime)s] %(lineno)d %(name)s %(levelname)s %(message)s",
+    level = logging.INFO
 )
 
 
