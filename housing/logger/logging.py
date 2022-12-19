903d2ac6d0809f916bd13fa7720dbfe61582f150
ashishs1407 <shimpi.ashish.me2@gmail.com>
2022-12-19 23:30:56 +0530

    update

---
+++ housing/logger/logging.py
@@ -0,0 +1,17 @@
+import os
+import logging
+from datetime import datetime
+from from_root import from_root
+
+log_file = f'datetime.now().strftime(%d_%m_%y_%H_%M_%S).log'
+log_path = os.path.join(from_root() , "log",log_file)
+os.mkdirs(log_path,exist_ok = True)
+log_file_path = os.path.join(log_path)
+
+logging.basicConfig(
+    filename= log_file_path,
+    format = '[%(asctime)s] %(lineno)d %(name)s %(levelname)s %(message)s',
+    level = logging.info
+)
+
+
